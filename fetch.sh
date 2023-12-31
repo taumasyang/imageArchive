451d17fc1750e640f230b1410e4f8f34f14e55a8
tauyoung <tauyoung@icloud.com>
2023-12-31 13:15:00 +0800

    Script v1.1

--- fetch.sh
+++ fetch.sh
@@ -1,19 +1,18 @@
 #!/bin/zsh
 idx=${1:-0}
-for mkt in {zh-cn,en-us,en-gb,en-ca,en-in,ja-jp,fr-fr,de-de,es-es,pt-br,it-it}
+for mkt in {ZH-CN,EN-US,EN-GB,EN-CA,EN-IN,JA-JP,FR-FR,DE-DE,ES-ES,PT-BR,IT-IT}
 do
 	image=$(curl -sG -d idx=$idx -d n=1 -d mkt=$mkt https://www.bing.com/HPImageArchive.aspx | xmllint --xpath '/images/image' -)
 	[[ -z $image ]] && continue
-	startdate=$(echo $image | xmllint --xpath '/image/startdate/text()' -)
-	url=$(echo $image | xmllint --xpath '/image/url/text()' -)
-	filename=${${url#*.}%%_*}
+	enddate=$(echo $image | xmllint --xpath '/image/enddate/text()' -)
+	urlBase=$(echo $image | xmllint --xpath '/image/urlBase/text()' -)
+	filename=${${urlBase#*.}%%_*}
 	[[ -e img/$filename.jpg ]] && continue
-	echo $image | xmllint --xpath '/image/headline/text()' - > desc/$filename.txt
-	echo $image | xmllint --xpath '/image/copyright/text()' - >> desc/$filename.txt
-	curl -so img/$filename.jpg www.bing.com/${url%%&*}
-	[[ $mkt == zh-cn ]] && ln -f img/$filename.jpg img/latest.jpg
+	echo "$enddate,$filename,$mkt,$(echo ${urlBase##*_} | grep -oE '[0-9]+'),\"$(echo $image | xmllint --xpath '/image/headline/text()' -)\",\"$(echo $image | xmllint --xpath '/image/copyright/text()' -)\"" >> metadata.csv
+	curl -so img/$filename.jpg www.bing.com${urlBase}_1920x1080.jpg
+	[[ $mkt == ZH-CN ]] && ln -f img/$filename.jpg img/latest.jpg
 done
 [[ $(git status --porcelain) ]] || exit
-git add desc img
-git commit -m "Fetch: $startdate"
+git add img metadata.csv
+git commit -m "Fetch: $enddate"
 git push
