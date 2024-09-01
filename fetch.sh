e17e79267246f208be801e577b74452e62455289
tauyoung <tauyoung@icloud.com>
2024-09-01 00:00:00 -0500

    Script v1.5

--- fetch.sh
+++ fetch.sh
@@ -1,19 +1,22 @@
 #!/bin/zsh
-# version 1.4
-local idx=${1:-0}
+# version 1.5
+local -i idx=${1:-0}
+[[ idx -gt 14 ]] && echo 'index too large' >&2 && exit $idx
+local -i n=1
+[[ idx -gt 7 ]] && n=$((idx-6))
 local mkt
-for mkt in {ZH-CN,EN-US,EN-GB,EN-CA,EN-IN,JA-JP,FR-FR,DE-DE,ES-ES,PT-BR,IT-IT}
+for mkt in {EN-US,JA-JP,ZH-CN,EN-IN,DE-DE,ES-ES,FR-FR,IT-IT,EN-GB,PT-BR,EN-CA}
 do
-	unset image
 	while [[ -z $image ]]
-	do local image=$(curl -sG -d idx=$idx -d n=1 -d mkt=$mkt https://www.bing.com/HPImageArchive.aspx)
+	do local image=$(curl -sG -d idx=$idx -d n=$n -d mkt=$mkt https://www.bing.com/HPImageArchive.aspx)
 	done
-	local enddate=$(echo $image | xmllint --xpath '/images/image/enddate/text()' -)
-	local urlBase=$(echo $image | xmllint --xpath '/images/image/urlBase/text()' -)
+	local enddate=$(echo $image | xmllint --xpath '//enddate/text()' - | head -n$n)
+	local urlBase=$(echo $image | xmllint --xpath '//urlBase/text()' - | head -n$n)
 	local filename=${${urlBase#*.}%%_*}
-	echo "$enddate,$filename,$mkt,$(echo ${urlBase##*_} | grep -oE '[0-9]+'),\"$(echo $image | xmllint --xpath '/images/image/headline/text()' -)\",\"$(echo $image | xmllint --xpath '/images/image/copyright/text()' -)\"" >> metadata/$mkt.csv
+	echo "$enddate,$filename,$mkt,$(echo ${urlBase##*_} | grep -oE '[0-9]+'),\"$(echo $image | xmllint --xpath '//headline/text()' - | head -n$n)\",\"$(echo $image | xmllint --xpath '//copyright/text()' - | head -n$n)\"" >> metadata/$mkt.csv
 	[[ -e img/$filename.jpg ]] || curl -so img/$filename.jpg https://www.bing.com${urlBase}_1920x1080.jpg
-	[[ $mkt == ZH-CN ]] && ln -f img/$filename.jpg img/latest.jpg
+	ln -f img/$filename.jpg img/latest.jpg
+	unset image
 done
 [[ $(git status --porcelain) ]] || exit
 git add img metadata
